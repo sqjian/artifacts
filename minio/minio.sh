f82bcbe302b8e48d205cd63a66b4d30aa7b5fdc5
jianshengqi <shengqi.jian@qq.com>
2020-12-17 16:01:55 +0800

    Update minio.sh

--- minio/minio.sh
+++ minio/minio.sh
@@ -3,4 +3,4 @@
 docker run --restart=always -d -p 9000:9000 \
   -e "MINIO_ACCESS_KEY=12345678" \
   -e "MINIO_SECRET_KEY=12345678" \
-  hub.iflytek.com/aiaas/minio server /data
+  registry.cn-hangzhou.aliyuncs.com/sqjian/artifacts:minio server /data
